83e430ff7f485ec97eab4690fb6fb05cf7387f5f
Hardik Goyal <hardikgoyals@gmail.com>
2026-03-21 15:20:50 -0700

    feat: add search results comment

--- pages/search.js
+++ pages/search.js
@@ -16,6 +16,7 @@ export default function Search() {
         <input type="text" name="query" />
         <button type="submit">Search</button>
       </form>
+      {/* render search results */}
       <div dangerouslySetInnerHTML={{ __html: searchResult }} />
     </div>
   );
